eb7a8a637334207ee7655ec39e95f32e9625ef0a
Chris Weyl <cweyl@alumni.drew.edu>
2015-03-12 09:45:35 -0700

    initial commit

---
+++ lib/MooseX/Meta/TypeConstraint/Mooish.pm
@@ -0,0 +1,19 @@
+package MooseX::Meta::TypeConstraint::Mooish;
+
+# ABSTRACT: The great new MooseX::Meta::TypeConstraint::Mooish!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
