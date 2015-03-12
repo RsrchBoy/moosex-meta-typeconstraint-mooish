5d332f65898995a95a2708103782b6ae9b6d8ed4
Chris Weyl <cweyl@alumni.drew.edu>
2015-03-12 11:20:20 -0700

    Initial add (well, kinda)

--- lib/MooseX/Meta/TypeConstraint/Mooish.pm
+++ lib/MooseX/Meta/TypeConstraint/Mooish.pm
@@ -3,17 +3,54 @@ package MooseX::Meta::TypeConstraint::Mooish;
 # ABSTRACT: The great new MooseX::Meta::TypeConstraint::Mooish!
 
 use Moose;
-use namespace::autoclean;
+use namespace::autoclean 0.24;
 
+extends 'Moose::Meta::TypeConstraint';
+with 'MooseX::TraitFor::Meta::TypeConstraint::Mooish';
 
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
 !!42;
 __END__
 
 =head1 SYNOPSIS
 
+    # easiest is via AttributeShortcuts
+    use MooseX::AttributeShortcuts 0.028;
+
+    has foo => (
+        is  => 'rw',
+        isa => sub { die unless $_[0] == 5 },
+    );
+
+    # or, the hard way
+    use MooseX::Meta::TypeConstraint::Mooish;
+
+    has foo => (
+        is  => 'rw',
+        isa => MooseX::Meta::TypeConstraint::Mooish->new(
+            constraint => sub { die unless $_[0] == 5 },
+        ),
+    );
+
 =head1 DESCRIPTION
 
+L<Moose type constraints|Moose::Meta::TypeConstraint> are expected to return
+true if the value passes the constraint, and false otherwise; L<Moo>
+"constraints", on the other hand, die if validation fails.
+
+This metaclass allows for Moo-style constraints; it will wrap them and
+translate their Moo into a dialect Moose understands.
+
+Note that this is largely to enable functionality in
+L<MooseX::AttributeShortcuts>; the easiest way use this metaclass is by using
+that package.  Also, as it's not inconceivable that this functionality may be
+desired in other constraint metaclasses, the bulk of this metaclass'
+functionality is implemented as
+L<a trait|MooseX::TraitFor::Meta::TypeConstraint::Mooish>.
+
 =head1 SEE ALSO
+
+MooseX::AttributeShortcuts
+MooseX::TraitFor::Meta::TypeConstraint::Mooish
 
 =cut
---
+++ lib/MooseX/TraitFor/Meta/TypeConstraint/Mooish.pm
@@ -0,0 +1,119 @@
+package MooseX::TraitFor::Meta::TypeConstraint::Mooish;
+
+# ABSTRACT: Handle Moo-style constraints
+
+use Moose::Role;
+use namespace::autoclean 0.24;
+use Try::Tiny;
+
+=attr original_constraint
+
+The original constraint CodeRef is stashed away here.
+
+=method original_constraint()
+
+Reader for the L</original_constraint> attribute; returns the original
+constraint as passed to new().
+
+=method has_original_constraint()
+
+Predicate for the L</original_constraint> attribute.
+
+=cut
+
+has original_constraint => (
+    is        => 'ro',
+    isa       => 'CodeRef',
+    writer    => '_set_original_constraint',
+    predicate => 'has_original_constraint',
+);
+
+=attr mooish
+
+If true, the constraint should be considered written in the style of L<Moo> constraints; that is, if an exception is thrown the
+constraint is considered to have failed; otherwise it passes.  Return values are ignored.
+
+Default is true.
+
+=method mooish()
+
+Reader for the L</mooish> attribute.
+
+=cut
+
+has mooish => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 1,
+);
+
+=before compile_type_constraint
+
+If L</mooish> is true, we wrap the L</original_constraint> in a sub that translates L<Moo> behaviors
+(die on fail; otherwise success) to L<Moose::Meta::TypeConstraint> expectations (false on fail; true on success).
+
+We stash the original constraint in L</original_constraint> (surprise!),
+and set the L<Moose::Meta::TypeConstraint/constraint|constraint attribute> to
+the wrapped constraint.
+
+=cut
+
+before compile_type_constraint => sub {
+    my $self = shift @_;
+
+    ### only wrap our given constraint iff we're supposed to be mooish...
+    return unless $self->mooish;
+
+    ### wrap our type constraint, and set it...
+    my $wrapped_constraint = $self->_wrap_constraint($self->constraint);
+    $self->_set_original_constraint($self->constraint);
+    $self->_set_constraint($wrapped_constraint);
+
+    return;
+};
+
+sub _wrap_constraint {
+    my ($self, $constraint) = @_;
+
+    # call the original constraint.  if it does not die, return true; if it
+    # does die, return false.  We might do something with the fail message
+    # down the road, but not right now.
+
+    return sub {
+        my @args = @_;
+        my $fail_msg = try {
+            local $_ = $args[0];
+            $constraint->(@args);
+            return;
+        }
+        catch {
+            return $_;
+        };
+
+        return !$fail_msg;
+    };
+}
+
+!!42;
+__END__
+
+=pod
+
+=for :stopwords mooish
+
+=head1 DESCRIPTION
+
+    # determining where this goes is left as an exercise for the reader
+    with 'MooseX::TraitFor::Meta::TypeConstraint::Mooish';
+    
+=head1 SYNOPSIS
+
+This trait implements the functionality described in
+L<MooseX::Meta::TypeConstraint::Mooish>, and you, dear reader, are encouraged
+to read about it over there.  Here we simply document the nuts and bolts.
+
+=head1 SEE ALSO
+
+MooseX::Meta::TypeConstraint::Mooish
+
+=cut
